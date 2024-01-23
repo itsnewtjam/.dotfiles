e03d6d9f4834cbda6e9fcc87d6d6d2491988f524
James <78121730+itsnewtjam@users.noreply.github.com>
2024-01-23 16:46:42 -0500

    add git-worktree

--- nvim/.config/nvim/lua/newt/plugins/telescope.lua
+++ nvim/.config/nvim/lua/newt/plugins/telescope.lua
@@ -2,6 +2,9 @@ return {
   'nvim-telescope/telescope.nvim',
   tag = '0.1.4',
   dependencies = { 'nvim-lua/plenary.nvim' },
+  config = function()
+    require('telescope').load_extension('git_worktree')
+  end,
   keys = {
     {"<leader>pf", "<cmd>Telescope find_files<cr>", desc = "project files"},
     {"<leader>pg", "<cmd>Telescope git_files<cr>", desc = "project git files"},
@@ -11,6 +14,20 @@ return {
         require('telescope.builtin').grep_string({ search = vim.fn.input("Grep > ") })
       end,
       desc = "project search"
+    },
+    {
+      "<leader>gw",
+      function()
+        require('telescope').extensions.git_worktree.git_worktrees()
+      end,
+      "git worktree"
+    },
+    {
+      "<leader>gwa",
+      function()
+        require('telescope').extensions.git_worktree.create_git_worktree()
+      end,
+      "git worktree add"
     },
   }
 }
